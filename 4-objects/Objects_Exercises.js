be6b1f1745fca28cb1ca71a9c6071d22d22db53f
Javier Gonzalez <javihergonzalez@gmail.com>
2017-10-10 20:35:35 -0700

    Update Objects_Exercises.js

--- 4-objects/Objects_Exercises.js
+++ 4-objects/Objects_Exercises.js
@@ -1,6 +1,6 @@
-// //   The function `clean` should accept an object as an argument and return a new
-// //   object that has all of the key/value pairs of its parameter except for those
-// //   that begin with `_`.
+// 1.  The function `clean` should accept an object as an argument and return a new
+//   object that has all of the key/value pairs of its parameter except for those
+//   that begin with `_`.
 
 var dirtyObject = {
     _fht: 192492,
@@ -21,10 +21,10 @@ function clean(obj) {
   }
 clean(dirtyObject); // => {name: "Alyssa P. Hacker", age: 26}
 
-// // 2. Write a function `removeOddValues` that takes an object as an argument and
-// //   returns an object with all key/value pairs removed for which the value holds
-// //   an *odd number*. You'll need to use the \`typeof\` operator to first check that
-// //   the values are numbers:
+// 2. Write a function `removeOddValues` that takes an object as an argument and
+//   returns an object with all key/value pairs removed for which the value holds
+//   an *odd number*. You'll need to use the \`typeof\` operator to first check that
+//   the values are numbers:
 
 function removeOddValues (object) {
   var noOddValues = {};
@@ -42,9 +42,9 @@ function removeOddValues (object) {
 
 removeOddValues (dirtyObject);
 
-// // 2. Write a function `countWords` that, when given a string as an argument,
-// //   returns an *object* where *keys* are the words in the string, and *values*
-// //   are the number of occurrences of that word within the string:
+// 3. Write a function `countWords` that, when given a string as an argument,
+//   returns an *object* where *keys* are the words in the string, and *values*
+//   are the number of occurrences of that word within the string:
 
 function countWords(s) {
   var obj = {};
@@ -62,7 +62,7 @@ countWords("hello hello hello"); // => {"hello": 2}
 countWords("Hello hello"); // => {"Hello": 1, "hello": 1}
 countWords("The quick brown brown"); // => {"The": 1, "quick": 1, "brown": 1}
 
-// 3. Write a function `countCharacters` that, when given a string as an argument,
+// 4. Write a function `countCharacters` that, when given a string as an argument,
 //   returns an object containing counts of the ocurrences of each *character* in
 //   the string.
 
@@ -80,7 +80,7 @@ function countCharacters(s) {
     }
 countCharacters("hello"); // => {"h": 1, "e": 1, "l": 2, "o": 1}
 
-// 4. Write a function `select` that accepts two arguments: an object and an
+// 5. Write a function `select` that accepts two arguments: an object and an
 //   array. The **array** should contain names of keys that will be *selected* from
 //   the object:
 
